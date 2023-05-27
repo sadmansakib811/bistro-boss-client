a8044f09098888363027db4385507d5b0e16e08f
Sadman Sakib <sadmansakib123450@gmail.com>
2023-05-28 00:12:21 +0200

    navbar added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import {
   } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../pages/Home/Home/Home";
+import Menu from "../pages/Menu/Menu/Menu";
 
  export const router = createBrowserRouter([
     {
@@ -13,7 +14,11 @@ import Home from "../pages/Home/Home/Home";
         {
             path:'/',
             element: <Home></Home>
-        }
+        },
+        {
+          path:'menu',
+          element: <Menu></Menu>
+         }
       ]
     },
   ]);
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const NavBar = () => {
+
     const navOptions = <>
-    <li><a>Item 555</a></li>
-    <li tabIndex={0}>
-        <a className="justify-between">
-            Parent
-            <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-        </a>
-        <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-        </ul>
-    </li>
-    <li><a>Item 3</a></li>
-</>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/menu">Our Menu</Link></li>
+        <li><Link to="/order/salad">Order Food</Link></li>
+        
+    </>
+
     return (
         <>
             <div className="navbar fixed z-10 bg-opacity-30 max-w-screen-xl bg-black text-white">
@@ -42,4 +36,4 @@ const Navbar = () => {
     );
 };
 
-export default Navbar;
+export default NavBar;
